183cf498ab6126178a643bf21d20e978a953996d
Neelapu Varshitha <101424994+Varshitha-Neelapu@users.noreply.github.com>
2024-03-23 19:37:47 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,8 +17,8 @@ response = None
 st.title('Bank Marketing Analysis and Modeling', anchor=None)
 
 st.write("""
-    - [Exploratory Data Analysis](https://github.com/dendihandian/bank-marketing-analysis/blob/main/bank-marketing.ipynb)
-    - [Dataset](https://www.kaggle.com/datasets/dhirajnirne/bank-marketing)
+    - [Exploratory Data Analysis](https://github.com/Varshitha-Neelapu/Bank-Marketing/edit/main/Bank_Marketing_with_ML.ipynb)
+    - [Dataset](https://archive.ics.uci.edu/static/public/222/bank+marketing.zip)
 """)
 
 with st.container():
